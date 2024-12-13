32e8efa068265966d8eb868a2cd1d59500f45361
Jared Frazier <frazier@iap-kborn.de>
2024-12-13 13:22:31 +0100

    loading registry works under the current scheme, requires .venv

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # registry
 
 https://simonwillison.net/2022/Apr/24/pip-install-github/
+
+build wheel!
+
+python -m build --wheel
--- mod/__init__.py
+++ mod/__init__.py
@@ -1,5 +1,44 @@
+import importlib.util
+import os 
+import pdb
+
+
 # this calls the register function and populates the registry
 import mod.plot 
 
-# TODO: here get the .register path, read it, and import modules
-# appropriately
+def import_from_path(module_name, file_path):
+    # Load the module specification
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    # Create a new module based on the specification
+    module = importlib.util.module_from_spec(spec)
+    # Execute the module in its own namespace
+    spec.loader.exec_module(module)
+    return module
+
+fpath = os.path.realpath(__file__)
+if ".venv" in fpath:
+    print("i'm a package of a project!")
+    project_dir = fpath.split(".venv")[0]
+    print(project_dir)
+    print("project dir:", project_dir)
+    register_fpath = os.path.join(project_dir, ".register")
+    if os.path.exists(register_fpath):
+        with open(register_fpath, "r") as fp:
+            for path_to_module_with_register_decorators in fp.readlines():
+                path_to_module_with_register_decorators = \
+                    path_to_module_with_register_decorators.strip()
+                # absolute path
+                if os.path.exists(path_to_module_with_register_decorators):
+                    import_path = path_to_module_with_register_decorators
+                # otherwise try relative path
+                elif os.path.exists(
+                    os.path.join(project_dir, 
+                        path_to_module_with_register_decorators)):
+                    import_path = os.path.join(
+                        project_dir, path_to_module_with_register_decorators) 
+                else:
+                    raise ValueError(f"{path_to_module_with_register_decorators} dne!") 
+                module_name = os.path.basename(import_path.rstrip(".py"))
+                import_from_path(module_name, import_path)       
+else:
+    print("i'm in development repo!") 
